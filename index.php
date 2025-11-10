d6c69cc5be1745e04b3d24c6a945ae9d50baecb9
Shamzbruv <98935666+Shamzbruv@users.noreply.github.com>
2025-11-10 18:52:30 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// Simple theme loader that outputs the existing static homepage HTML.
+// Do NOT edit index.html for WordPress; keep all your custom code there.
+readfile(__DIR__ . '/index.html');
